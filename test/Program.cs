da728de7c7da41e536b6fed919bb2dc1a5a6328e
Alexander Schulz <alexander.schulz@digitalscope.de>
2020-12-05 00:37:29 +0100

    Initial Commit

---
+++ test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.;
+
+namespace test
+{
+    class Program : Draw
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
